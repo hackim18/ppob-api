5fa89a3c2ed4b9c36cca9ce97f9e6578c7853679
Hackim18 <khakim668@gmail.com>
2024-08-29 01:11:58 +0700

    refactor: refactoring setup main app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
@@ -6,6 +7,4 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World!" });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+module.exports = app;
