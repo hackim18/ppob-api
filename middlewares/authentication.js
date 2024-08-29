9477080b197fe2b9b2a10e811236e59a3e738294
Hackim18 <khakim668@gmail.com>
2024-08-30 02:46:34 +0700

    feat: add authentication

---
+++ middlewares/authentication.js
@@ -0,0 +1,35 @@
+const { verifyToken } = require("../helpers/jwt");
+const { sequelize } = require("../models");
+
+async function authentication(req, res, next) {
+  try {
+    const access_token = req.headers.authorization;
+
+    if (!access_token) throw { name: "Unauthenticated", message: "Access token is required" };
+    const [type, token] = access_token.split(" ");
+
+    if (type !== "Bearer") throw { name: "Unauthenticated" };
+    const payload = verifyToken(token);
+
+    const { email } = payload;
+
+    const user = await sequelize.query(
+      'SELECT * FROM "Users" WHERE email = :email LIMIT 1',
+      {
+        replacements: { email },
+        type: sequelize.QueryTypes.SELECT,
+      },
+      { email }
+    );
+
+    if (!user) throw { name: "Unauthenticated" };
+
+    req.user = { email: user[0].email };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = authentication;
